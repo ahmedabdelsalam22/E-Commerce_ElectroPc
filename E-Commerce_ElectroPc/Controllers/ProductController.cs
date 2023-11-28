8019add0777470a66728def033622ea78969987f
prAhmed20 <ttbyqatandrwydmhkrh@gmail.com>
2023-11-29 00:20:58 +0200

    create ProductController and gett all products in index action

---
+++ E-Commerce_ElectroPc/Controllers/ProductController.cs
@@ -0,0 +1,30 @@
+﻿using AutoMapper;
+using ElectroPc_Models;
+using ElectroPc_Models.Dtos;
+using ElectroPc_Services.UnitOfWork;
+using Microsoft.AspNetCore.Mvc;
+
+namespace E_Commerce_ElectroPc.Controllers
+{
+    public class ProductController : Controller
+    {
+        private readonly IUnitOfWork _unitOfWork;
+        private readonly IMapper _mapper;
+
+        public ProductController(IUnitOfWork unitOfWork, IMapper mapper)
+        {
+            _unitOfWork = unitOfWork;
+            _mapper = mapper;
+        }
+
+        public async Task<IActionResult> Index()
+        {
+            IEnumerable<Product> products = await _unitOfWork.productRepository.GetAllAsync();
+
+            IEnumerable<ProductDto> productDtos = _mapper.Map<List<ProductDto>>(products);
+
+            return View(productDtos);
+        }
+
+    }
+}
---
+++ ElectroPc_Models/Dtos/ProductDto.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ElectroPc_Models.Dtos
+{
+    public class ProductDto
+    {
+        public int ProductId { get; set; }
+        public string Name { get; set; }
+        public string Brand { get; set; }
+        public string Processor { get; set; }
+        public int RamSizeGB { get; set; }
+        public string StorageType { get; set; }
+        public int StorageCapacityGB { get; set; }
+        public double DisplaySizeInches { get; set; }
+        public string DisplayResolution { get; set; }
+        public string GraphicsCard { get; set; }
+        public string OperatingSystem { get; set; }
+        public double BatteryLifeHours { get; set; }
+        public double WeightKg { get; set; }
+        public string Dimensions { get; set; }
+        public bool IsTouchscreen { get; set; }
+        public bool HasWebcam { get; set; } 
+    }
+}
