db0058b5325fa37cb8380899291a5016309781bf
prAhmed20 <ttbyqatandrwydmhkrh@gmail.com>
2023-11-28 23:58:05 +0200

    create IProductRepository and impl

---
+++ ElectroPc_Services/Repositories/ProductRepository.cs
@@ -0,0 +1,25 @@
+﻿using ElectroPc_DataAccess;
+using ElectroPc_Models;
+using ElectroPc_Services.IRepositories;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ElectroPc_Services.Repositories
+{
+    public class ProductRepository : GenericRepository<Product>, IProductRepository
+    {
+        private readonly ApplicationDbContext _dbContext;
+        public ProductRepository(ApplicationDbContext dbContext) : base(dbContext)
+        {
+            _dbContext = dbContext;
+        }
+
+        public void Update(Product product)
+        {
+            _dbContext.Update(product);
+        }
+    }
+}
